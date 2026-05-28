8a2b473ddee5a6123ae69aeaa7a5150bafc6ae2c
Antonio <amrodriguezjarote@gmail.com>
2026-05-28 13:55:43 +0200

    add supabase connection

--- src/app/core/supabase/supabase.client.ts
+++ src/app/core/supabase/supabase.client.ts
@@ -2,6 +2,14 @@ import { createClient } from '@supabase/supabase-js';
 
 import { environment } from '../../../environments/environment';
 
+const supabaseClient = createClient(environment.supabaseUrl, environment.supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    persistSession: true,
+  },
+});
+
 export function createSupabaseClient() {
-  return createClient(environment.supabaseUrl, environment.supabaseAnonKey);
+  return supabaseClient;
 }
